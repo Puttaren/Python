655213d3111811bdf6ef93e3bc2471987262cada
Puttaren <michael@intertec.se>
2025-08-27 10:34:29 +0200

    Ny fil

---
+++ Hello2.py
@@ -0,0 +1,9 @@
+while True:
+    namn = input("What is your name? ").strip()
+    namn2 = input("Vad sade du? ").strip()
+
+    if namn2 == namn:
+        print(f"Hello, {namn}. Welcome to Python")
+        break   
+    else:
+        print("Lär dig skriva\n")
