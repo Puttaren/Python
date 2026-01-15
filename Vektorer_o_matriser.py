c5af3ea5bfe4b474feb5f7d863e840e995105b4d
Puttaren <michael@intertec.se>
2026-01-15 22:15:22 +0100

    Lägger till Python-experimenterande med matteuppgifter samt uppdatering av övningsuppgiftena del två.

---
+++ Vektorer_o_matriser.py
@@ -0,0 +1,11 @@
+# Genomgång av matteuppgifterna i “Matematik från yrkeshögskolan”, kapitel 10
+# Redan gjort 
+
+# Använd pandas och numpy som verktyg
+import pandas as pd
+import numpy as np
+
+# Hämta in Excel-filen
+file_path = "D:\Downloads\Övningsuppgifter YH Matematik.xlsx"
+
+#L 
